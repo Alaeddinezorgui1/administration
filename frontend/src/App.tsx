2485a964e2bf361ab7c674abf70e4375e6441977
Alaeddinezorgui1 <ala.eddin.zorgui@gmail.com>
2024-11-01 19:55:05 -0400

    add front page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,13 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import User from './pages/user'
 
-const App: React.FC = () => {
-  const [username, setUsername] = useState('');
 
-  const addUser = async () => {
-    try {
-      await axios.post('http://localhost:5000/users/add', { username });
-      alert('User added successfully!');
-    } catch (error) {
-      console.error(error);
-      alert('Error adding user.');
-    }
-  };
 
+
+const App: React.FC = () => {  
   return (
     <div>
-      <h1>Add User</h1>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      <button onClick={addUser}>Add</button>
+      <User/>
     </div>
   );
 };
